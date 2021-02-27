8cada9906fe3f6575c0207cf485702878bbd6271
Johnathan Wilson <johnathan@kellerson.co.uk>
2021-02-27 10:15:31 +0000

    Extending the base template to additional pug files using Blocks

--- app.js
+++ app.js
@@ -75,8 +75,21 @@ app.use((req, res, next) => {
 // 3) Routes
 app.get('/', (req, res) => {
   res.status(200).render('base', {
+    title: 'Exciting tours for adventurous people',
     tour: 'The Forest Hiker',
     user: 'John',
+  });
+});
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
   });
 });
 
