493bcad27bc7fbf107ffbefb15e68bdb8eef4bf0
Johnathan Wilson <johnathan@kellerson.co.uk>
2021-02-27 13:41:58 +0000

    Implementing mapbox into the client side.

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -5,3 +5,51 @@ console.log('hello from the client side');
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 
 console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoiam9obmFyY2hlcjk2NSIsImEiOiJja2xucXJtcGswbG8yMnFubm14Nnd4MWFyIn0.90YonsvpcaEVe_-_rOp0ig';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v10',
+  scrollZoom: false,
+  //   center: [-118.113491, 34.111745],
+  //   zoom: 10,
+  //   interactive: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // Create Marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add Marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add Popup
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
